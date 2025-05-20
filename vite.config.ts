31129dc6d9d63ab7493996e6b31d83fb0e718969
David Noorda <david.noorda@litmos.com>
2025-05-20 15:47:15 +1000

    Update Vite base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tres-comas-wheel/', // Set base for GitHub Pages deployment
   plugins: [react()],
 })
